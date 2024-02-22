49db54e2dd112b5a5f38464588387af44b5467dd
vhenriquebm <vhenriquebm@gmail.com>
2024-02-22 09:26:17 -0300

    Ajustes no layout, inclusão do ViewBinding

--- app/src/main/java/com/example/motivation/MainActivity.kt
+++ app/src/main/java/com/example/motivation/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.motivation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.motivation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
+
+
 }
